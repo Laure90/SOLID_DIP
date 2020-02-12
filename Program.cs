fa5a06f50c04a20a7ff68375a2a7631f4f9d43c4
Laure90 <58706312+Laure90@users.noreply.github.com>
2020-02-12 08:57:35 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+using System.IO;
+
+namespace DIP
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            DataCommand datacommand = new Terminal();
+            while (!datacommand.Exited)
+            {
+                Command command = datacommand.PromptCommand();
+                datacommand.ExecuteCommand(command);
+            }
+        }
+    }
+
+
+
+    
+}
